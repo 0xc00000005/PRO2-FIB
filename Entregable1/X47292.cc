bf85ad70e0c66dc153e6b0c9be49f7c29b1fca5e
0xc00000005 <marti.alvarez.mato@estudiantat.upc.edu>
2024-10-01 18:43:28 +0200

    comentarios añadidos

--- Entregable1/X47292.cc
+++ Entregable1/X47292.cc
@@ -2,7 +2,23 @@
 #include <stack>
 using namespace std;
 
-void sort_stack(stack<int>& inicial, stack<int>& ordenado);
+void sort_stack(stack<int>& inicial, stack<int>& ordenado) {
+	while (not inicial.empty()) {
+		// Cogemos elemento de la pila inicial y lo eliminamos
+		int elem = inicial.top();
+		inicial.pop();
+		// Si hay algo en la ordenada y el elemento que hemos cogido es mayor
+		// Volvemos a ponerlo en la inicial y vaciamos la ordenada
+		while (not ordenado.empty() and ordenado.top() < elem) {
+			inicial.push(ordenado.top());
+			ordenado.pop();
+		}
+		// Ponemos el elemento en la ordenada
+		ordenado.push(elem);
+		// Asi conseguimos que si se pone algo en la ordenada sea siempre
+		// menor que el que ya estaba, oredeando la pila
+	}
+}
 
 void leer(stack<int>& p) {
 	int n;
