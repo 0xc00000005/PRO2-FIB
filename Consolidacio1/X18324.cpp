b65a85e3a13a9adf3e95d0e95980a3034c11be0a
0xc00000005 <marti.alvarez.mato@estudiantat.upc.edu>
2024-10-26 21:09:35 +0200

    sin comentarios

--- Consolidacio1/X18324.cpp
+++ Consolidacio1/X18324.cpp
@@ -7,14 +7,14 @@ bool isMatchingPair(char open, char close) {
     return (open == '(' && close == ')') || (open == '[' && close == ']');
 }
 
-int main() {
+int main(){
     string input;
-    while (cin >> input) {
+    while (cin >> input){
         stack<int> pila;
         int maxLength = 0;
         int lastValidIndex = -1;
 
-        for (int i = 0; i < input.length(); ++i) {
+        for (int i = 0; i < input.length(); ++i){
             char c = input[i];
             if (c == '(' || c == '[') {
                 pila.push(i);
@@ -22,14 +22,11 @@ int main() {
                 if (!pila.empty() && isMatchingPair(input[pila.top()], c)) {
                     pila.pop();
                     if (pila.empty()) {
-                        // Si la pila está vacía, la secuencia es válida desde lastValidIndex hasta i
                         maxLength = max(maxLength, i - lastValidIndex);
                     } else {
-                        // Si la pila no está vacía, la secuencia válida está entre el último elemento de la pila y i
                         maxLength = max(maxLength, i - pila.top());
                     }
                 } else {
-                    // Si no hay un par válido, actualizamos el índice del último carácter no válido
                     lastValidIndex = i;
                 }
             }
