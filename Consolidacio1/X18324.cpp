74991e675474a1adce53f4eedb36d38e9e7c09e0
0xc00000005 <marti.alvarez.mato@estudiantat.upc.edu>
2024-10-26 18:30:18 +0200

    X18324

--- Consolidacio1/X18324.cpp
+++ Consolidacio1/X18324.cpp
@@ -3,13 +3,35 @@
 #include <stack>
 using namespace std;
 
+bool isMatchingPair(char open, char close) {
+    return (open == '(' && close == ')') || (open == '[' && close == ']');
+}
+
 int main(){
     string input;
-    int i;
     while (cin >> input){
-        stack<char> pila;
-        for (char& c : str){
+        stack<int> pila;
+        int maxLength = 0;
+        int lastValidIndex = -1;
 
+        for (int i = 0; i < input.length(); ++i){
+            char c = input[i];
+            if (c == '(' || c == '[') {
+                pila.push(i);
+            } else if (c == ')' || c == ']') {
+                if (!pila.empty() && isMatchingPair(input[pila.top()], c)) {
+                    pila.pop();
+                    if (pila.empty()) {
+                        maxLength = max(maxLength, i - lastValidIndex);
+                    } else {
+                        maxLength = max(maxLength, i - pila.top());
+                    }
+                } else {
+                    lastValidIndex = i;
+                }
+            }
         }
+
+        cout << maxLength << endl;
     }
 }
