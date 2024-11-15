49aff116fbf613a8a87968160828e6007a628673
0xc00000005 <marti.alvarez.mato@estudiantat.upc.edu>
2024-11-15 16:16:52 +0100

    entregable3 initial commit

---
+++ Entregable3/U48948/solucio.cc
@@ -0,0 +1,15 @@
+#include "Cjt_estudiants.hh"
+
+int Cjt_estudiants::posicio_nota(double nota_b) const {
+  
+}
+
+void Cjt_estudiants::afegir_estudiant(const Estudiant &est, bool &trobat)
+{
+  
+}
+
+void Cjt_estudiants::escriure_i(double nota_i, double nota_s) const
+{
+  
+}
