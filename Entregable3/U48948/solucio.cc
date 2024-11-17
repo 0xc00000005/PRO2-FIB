df1f86997b8c0033a2b893e28c4482737943bd24
0xc00000005 <marti.alvarez.mato@estudiantat.upc.edu>
2024-11-17 22:13:12 +0100

    comit

--- Entregable3/U48948/solucio.cc
+++ Entregable3/U48948/solucio.cc
@@ -6,33 +6,54 @@ int Cjt_estudiants::posicio_nota(double nota_b) const {
       o igual que nota_b a vest[0...nest-1]. Si no hi ha estudiants amb 
      nota major o igual que nota_b, el resultat és nest. */
   for (int i = 0; i < nest; ++i) {
-    if (vest[i].consultar_nota() >= nota_b) {
+    if (vest[i].te_nota() && vest[i].consultar_nota() >= nota_b) {
       return i;
     }
   }
   return nest;
 }
 
-void Cjt_estudiants::afegir_estudiant(const Estudiant &est, bool &trobat)
-{
-  if (nest >= MAX_NEST) return; // Ensure we do not exceed the maximum number of students
+void Cjt_estudiants::afegir_estudiant(const Estudiant& est, bool& trobat) {
   int dni = est.consultar_DNI();
-  int pos = cerca_dicot_per_dni(vest, 0, nest-1, dni);
-  trobat = (pos < nest && (vest[pos].consultar_DNI() == dni));
-  if (not trobat) {
-    // Shift elements to the right to make space for the new student
-    for (int i = nest; i > pos; --i) {
-      vest[i] = vest[i-1];
+  trobat = false;
+  int pos;
+
+  if (est.te_nota()) {
+    // Search in the range of students with grades
+    pos = cerca_dicot_per_dni(vest, primer_est_amb_nota, nest - 1, dni);
+    if (pos < nest && vest[pos].consultar_DNI() == dni) {
+      trobat = true;
+    } else {
+      // Shift elements to the right to make space for the new student
+      for (int i = nest; i > pos; --i) {
+        vest[i] = vest[i - 1];
+      }
+      vest[pos] = est;
+      ++nest;
     }
-    vest[pos] = est; // Insert the new student at the correct position
-    nest++; // Increment the number of students
+  } else {
+    // Search in the range of students without grades
+    pos = cerca_dicot_per_dni(vest, 0, primer_est_amb_nota - 1, dni);
+    if (pos < primer_est_amb_nota && vest[pos].consultar_DNI() == dni) {
+      trobat = true;
+    } else {
+      // Shift elements to the right including students with grades
+      for (int i = nest; i > pos; --i) {
+        vest[i] = vest[i - 1];
+      }
+      vest[pos] = est;
+      ++nest;
+      ++primer_est_amb_nota; // Update the index of first student with grade
+    }
   }
 }
 
 void Cjt_estudiants::escriure_i(double nota_i, double nota_s) const {
   int i = posicio_nota(nota_i);
-  while (i < nest && vest[i].consultar_nota() <= nota_s) {
-    vest[i].escriure();
+  while (i < nest) {
+    if (vest[i].te_nota() && vest[i].consultar_nota() <= nota_s) { // Check if the student has a grade and if the grade is within the range
+      vest[i].escriure();
+    }
     ++i;
   }
 }
